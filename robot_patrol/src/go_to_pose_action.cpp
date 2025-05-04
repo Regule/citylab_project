3271b33d2285f9795215ba26306c06a351741995
Regule <regule@runbox.com>
2025-05-04 07:33:48 +0000

    Changed velocity publishing to work better with real robot

--- robot_patrol/src/go_to_pose_action.cpp
+++ robot_patrol/src/go_to_pose_action.cpp
@@ -35,13 +35,17 @@ public:
 private:
   rclcpp_action::Server<GoToPose>::SharedPtr action_server_;
 
+  rclcpp::TimerBase::SharedPtr effector_update_clock_;
+
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_sub_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_pub_;
 
   rclcpp::CallbackGroup::SharedPtr action_callback_group_;
   rclcpp::CallbackGroup::SharedPtr sensors_callback_group_;
+  rclcpp::CallbackGroup::SharedPtr effector_callback_group_;
 
   NaiveGoto naive_goto_;
+  geometry_msgs::msg::Twist cmd_vel_;
 
   void update_odometry_(const nav_msgs::msg::Odometry::SharedPtr msg);
 
@@ -52,18 +56,27 @@ private:
   cancel_handler_(const std::shared_ptr<GoalHandleGoToPose> goal_handle);
   void accepted_handler_(const std::shared_ptr<GoalHandleGoToPose> goal_handle);
   void execute_(const std::shared_ptr<GoalHandleGoToPose> goal_handle);
+  void update_effectors_();
 };
 
 GoToActionServer::GoToActionServer(const rclcpp::NodeOptions &options)
     : Node("distance_action_server", options) {
   using namespace std::placeholders;
+
   action_callback_group_ =
       this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
   sensors_callback_group_ =
       this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  effector_callback_group_ =
+      this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
 
-  naive_goto_.set_linear_pid(SimplePID(0.3, 0.001, 0.01));
-  naive_goto_.set_angular_pid(SimplePID(0.7, 0.001, 0.01));
+  naive_goto_.set_linear_pid(SimplePID(0.3, 0.0001, 0.01));
+  naive_goto_.set_angular_pid(SimplePID(0.7, 0.0001, 0.01));
+
+  effector_update_clock_ = this->create_wall_timer(
+      std::chrono::milliseconds(100),
+      std::bind(&GoToActionServer::update_effectors_, this),
+      effector_callback_group_);
 
   // rclcpp_action::ServerOptions action_server_options;
   // action_server_options.callback_group = action_callback_group_;
@@ -104,6 +117,8 @@ GoToActionServer::goal_handler_(const rclcpp_action::GoalUUID &uuid,
 rclcpp_action::CancelResponse GoToActionServer::cancel_handler_(
     const std::shared_ptr<GoalHandleGoToPose> goal_handle) {
   RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+  cmd_vel_ = geometry_msgs::msg::Twist();
+  velocity_pub_->publish(geometry_msgs::msg::Twist());
   (void)goal_handle;
   return rclcpp_action::CancelResponse::ACCEPT;
 }
@@ -123,7 +138,7 @@ void GoToActionServer::execute_(
   const auto goal = goal_handle->get_goal();
   auto feedback = std::make_shared<GoToPose::Feedback>();
   auto result = std::make_shared<GoToPose::Result>();
-  rclcpp::Rate loop_rate(1);
+  rclcpp::Rate loop_rate(1); // Changes requires retuning PID's do not touch.
 
   while (!naive_goto_.target_reached()) {
     if (goal_handle->is_canceling()) {
@@ -132,19 +147,22 @@ void GoToActionServer::execute_(
       RCLCPP_INFO(this->get_logger(), "Goal canceled");
       return;
     }
-    velocity_pub_->publish(naive_goto_.get_cmd_vel().to_Twist());
+    cmd_vel_ = naive_goto_.get_cmd_vel().to_Twist();
     feedback->current_pos = naive_goto_.get_position().to_Pose2D();
     goal_handle->publish_feedback(feedback);
     loop_rate.sleep();
   }
 
   if (rclcpp::ok()) {
+    cmd_vel_ = geometry_msgs::msg::Twist();
     velocity_pub_->publish(geometry_msgs::msg::Twist());
     result->status = true;
     goal_handle->succeed(result);
     RCLCPP_INFO(this->get_logger(), "Goal succeeded");
   }
 }
+
+void GoToActionServer::update_effectors_() { velocity_pub_->publish(cmd_vel_); }
 
 //-------------------------------------------------------------------------------------------
 //                                       MAIN
